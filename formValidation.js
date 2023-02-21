4475f6abdd4ebda208f372f0032c1b53126a2af6
Jonas00016 <65242536+Jonas00016@users.noreply.github.com>
2023-02-21 09:42:15 +0100

    Add files via upload

---
+++ formValidation.js
@@ -0,0 +1,14 @@
+const USERNAME = 'CodingMaster42'
+const PASSWORD = 'TopSecret123!'
+const EMAIL = 'coding@master.com'
+const EMAIL_CONFIRMATION = 'coding@master.com'
+
+// Sample usernames
+const takenUsernames = [
+    'Webmaster1',
+    'P123JS456',
+]
+
+const validateForm = (username, password, email, emailConfirmation) => {}
+
+console.log(validateForm(USERNAME, PASSWORD, EMAIL, EMAIL_CONFIRMATION))
